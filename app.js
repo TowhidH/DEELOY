0bdbb61533ba02e4c1e0302af7b8926fd8417bb0
Towhid_H <towhidhussain4@gmail.com>
2022-09-05 19:34:49 +0600

    delloy

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const app = express();
-
-
-app.get("/" , (req  , res)=>{
-
-    res.send("<h1>towhids home page</h1>")
-})
+const path = require("path")
+app.use(express.static(path.join(__dirname , "./public")))
 
 
 
